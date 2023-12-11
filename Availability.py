e138287aeff298394f5356a815ccedc2293bd51a
soydas <34300749+soydas@users.noreply.github.com>
2023-12-11 14:13:28 +0300

    Update Availability.py

--- Availability.py
+++ Availability.py
@@ -1,79 +1,105 @@
-# Author S.G. 22.01.2022 Availability Check
-from urllib.request import Request, urlopen
-from urllib.error import URLError, HTTPError
+# Author S.G. 22.01.2022 Availability Check (Mevcudiyet:)
+# https://github.com/soydas/Availability
+
+import os
+import ssl
+import sys
 import urllib.request
 from datetime import datetime
 from socket import timeout
-import time
-import sys
-import ssl
-import os
+from urllib.error import URLError, HTTPError
+from urllib.request import Request
 
+from colorama import Fore, Style
+from tabulate import tabulate
+
+# Bu kod gereklidir, silinirse Status "Unreachable" olur.
 try:
     _create_unverified_https_context = ssl._create_unverified_context
 except AttributeError:
-    # Legacy Python that doesn't verify HTTPS certificates by default
+    # Varsayılan olarak HTTPS sertifikalarını doğrulamayan Eski Python için
     pass
 else:
-    # Handle target environment that doesn't support HTTPS verification
+    # HTTPS doğrulamasını desteklemeyen hedef ortamı için
     ssl._create_default_https_context = _create_unverified_https_context
 
+url_listem = "list.txt"
+script_dizini = os.path.dirname(os.path.abspath(__file__))
+dosya_yolu = os.path.join(script_dizini, url_listem)
 
 while True:
 
-    f = open("list.txt", "r")
+    with open(dosya_yolu, "r") as f:
 
-    for line in f:
-        if line[0] == '#':
-            continue
-        print(len(line) * '_')
+        table = []
 
-        if line.strip() == "":
-            continue
+        for line in f:
+            if line[0] == '#':
+                continue
 
-        req = Request(line)
+            if line.strip() == "":
+                continue
 
-        try:
-            response = urllib.request.urlopen(req, timeout=15)
-            if response.status == 200:
-                print(line + "\nResponse : " + str(response.status) + " OK.")
+            req = Request(line)
+            opener = urllib.request.build_opener()
+            # 'User-agent', 'Python-urllib/3.11' --> 'User-Agent', 'Mozilla/5.0') dönüştürür.
+            opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
+            row = [line]
 
-        except timeout as e:
-            print(line + '-->socket timed out')
-            tarih = datetime.now().strftime('%Y-%m-%d')
-            print(tarih)
-            zaman = datetime.now().strftime('%Y-%m-%d %H %M %S')
-            print(zaman)
-            print(os.getcwd())
-            fh = open("log_" + tarih + ".txt", mode="a+")
-            fh.write(zaman + " " + line + "-->socket timed out\n")
-            fh.close()
+            try:
+                response = opener.open(req, timeout=15)
+                if response.status == 200:
+                    row.append(Fore.GREEN + "Response : " + str(response.status) + " OK." + Style.RESET_ALL)
 
-        except HTTPError as e:
-            print(line + '-->Sunucu isteği yerine getiremedi.')
-            print('Hata : ', e.code)
-            tarih = datetime.now().strftime('%Y-%m-%d')
-            zaman = datetime.now().strftime('%Y-%m-%d %H %M %S')
-            print(os.getcwd())
-            fh = open("log_" + tarih + ".txt", mode="a+")
-            fh.write(zaman + " " + line + "-->Sunucu isteği yerine getiremedi.\n")
-            fh.close()
+            except timeout as e:
+                row.append(Fore.RED + "Socket timed out" + Style.RESET_ALL)
+                tarih = datetime.now().strftime('%Y-%m-%d')
+                zaman = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                fh = open(script_dizini + "\\" + "log_" + tarih + ".txt", mode="a+")
+                fh.write(zaman + " " + line.strip() + " -> Zaman aşımı.\n")
+                fh.close()
 
-        except URLError as e:
-            print(line + '\n-->Ulaşılamıyor.')
-            print('Hata : ', e.reason)
-            tarih = datetime.now().strftime('%Y-%m-%d')
-            zaman = datetime.now().strftime('%Y-%m-%d %H %M %S')
-            print(os.getcwd())
-            fh = open("log_" + tarih + ".txt", mode="a+")
-            fh.write(zaman + " " + line + "-->Ulaşılamıyor.\n")
-            fh.close()
+            except HTTPError as e:
+                row.append(Fore.RED + "Server request failed" + Style.RESET_ALL)
+                # row.append("Error: " + str(e.code))
+                tarih = datetime.now().strftime('%Y-%m-%d')
+                zaman = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                fh = open(script_dizini + "\\" + "log_" + tarih + ".txt", mode="a+")
+                fh.write(zaman + " " + line.strip() + " -> Sunucu isteği yerine getiremedi.\n")
+                fh.close()
 
-        except ValueError as e:
-            print(line + '-->list.txt\'de uygun olmayan link mevcut!')
-            print('Hata : ', e.reason)
-        else:
+            except URLError as e:
+                row.append(Fore.RED + f"URL Hatası / Ulaşılamıyor" + Style.RESET_ALL)
+                # row.append("Error: " + str(e.reason))
+                tarih = datetime.now().strftime('%Y-%m-%d')
+                zaman = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                fh = open(script_dizini + "\\" + "log_" + tarih + ".txt", mode="a+")
+                fh.write(zaman + " " + line.strip() + f" -> URL Hatası: {e}.\n")
+                fh.close()
+
+            except ValueError as e:
+                print(Fore.RED + "list.txt içinde yanlış link var" + Style.RESET_ALL)
+
+            except ConnectionResetError as e:
+                row.append(Fore.RED + "Existing connection forcibly closed by a remote host" + Style.RESET_ALL)
+                tarih = datetime.now().strftime('%Y-%m-%d')
+                zaman = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                fh = open(script_dizini + "\\" + "log_" + tarih + ".txt", mode="a+")
+                fh.write(zaman + " " + line.strip() + " -> Varolan bir bağlantı"
+                                                      " uzaktaki bir ana bilgisayar tarafından zorla kapatıldı.\n")
+                fh.close()
+                break
+
+            except KeyboardInterrupt:
+                print(Fore.YELLOW + "bye! ;)" + Style.RESET_ALL)
+                sys.exit()
+
+            table.append(row)
             sys.stdout.flush()
+
     f.close()
-    print('15sn bekle')
-    time.sleep(15)
+    os.system('cls')
+    print(tabulate(table, headers=[Fore.BLUE + "URL", f"Status  {datetime.now().strftime('T:%H:%M:%S')}" + Style.RESET_ALL], tablefmt="grid"))
+
+    # print('10sn bekle')
+    # time.sleep(10)
